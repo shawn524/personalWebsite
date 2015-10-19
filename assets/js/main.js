f038e8feb7a9e5464259304b5e78965347a19d8c
Shawn Hanna <shawn524@gmail.com>
2015-10-19 11:50:10 -0400

    Added some random text strings on #about load

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ $("#about").hide();
 // Show and hide about me section when face is clicked
 $('#face').click(function() {
 	$("#about").slideToggle();
+	newGreet();
 });
 // Can also hide it from within #about
 $('#about').click(function() {
@@ -22,3 +23,14 @@ $('#face').hover(function() {
 }, function() {
     this.src = 'assets/img/closed.png';
 });
+
+// Display random greeting and name 
+var greetArr = ["Hey there", "What's up", "Welcome", "How's it going", "Sup fam"];
+var nameArr = ["My name is Shawn", "Shawn here", "You got Shawn", "Now with 12% more Shawn", "I'm Shawn"];
+function rand(arg) {
+	return arg[Math.floor(Math.random()*arg.length)]
+}
+function newGreet() {
+  $('#greeting').html(rand(greetArr))
+  $('#name').html(rand(nameArr))
+}
