1cb7e25592d92a3bd54a82b7c2ec389369081bce
Shawn <shawn524@gmail.com>
2016-03-16 12:05:32 -0400

    Updated css and changed modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
-
   // Fade in div elements on page load
   $('.divfade').each(function(i) {
     $(this).fadeOut(0).delay(1000*i).fadeIn(1850);
   });
 
-  // Hide about me section on load
-  $("#about").hide();
-
-  // Show and hide about me section when face is clicked
-  $('#face').click(function() {
-    $("#about").slideToggle();
-    newGreet();
+  $("#face").animatedModal({
+    animatedIn:'bounceInUp',
+    animatedOut:'bounceOutDown',
+    color:'#52616a',
   });
-  // Can also hide it from within #about
-  $('#about').click(function() {
-    $("#about").slideToggle();
-  });
-
 
   // Flip between open and closed mouth images
   $('#face').hover(function() {
